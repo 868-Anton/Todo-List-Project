000c0e95526a5f8b5d4e6c44f9c2ee2f100311bf
= <anton.graham.2011@gmail.com>
2022-05-30 10:29:02 -0400

    Remove connectToDb func

--- functions.php
+++ functions.php
@@ -3,15 +3,6 @@
 require 'Task.php';
 
 
-function connectToDb(){
-
-  try{
-    return new PDO('mysql:host=127.0.0.1;dbname=mytodo','root','');
-  }catch(PDOException $e)
-      {
-          die($e->getMessage());
-      }
-}
 
 function selectAll($pdo){
   $statement = $pdo->prepare('select * from todos');
