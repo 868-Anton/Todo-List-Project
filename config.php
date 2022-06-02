cf498e54a3492d8e55fdedd2e3f3be10a71d1bb8
= <anton.graham.2011@gmail.com>
2022-06-01 21:48:34 -0400

    Create config.php and update connection string and bootstrap.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+return [
+  'database'=>[
+    'connection'=>'mysql:host=127.0.0.1;',
+    'name'=>'dbname=mytodo',
+    'username'=>'root',
+    'password'=>'',
+    'options'=>[
+    ]
+
+  ]
+];
